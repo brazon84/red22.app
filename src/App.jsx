4ba1b42c9045bbed41d0a36ac412e81d003262a2
Jose Gregorio Brazon Lopez <abg.brazon@gmail.com>
2023-12-16 10:52:56 -0400

    cambio2

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,9 @@ function App() {
     <Informacion />
     <Contact />
     <Footer /> */}
+    <div className='formato'>
+    <h1 className='construccion text-center mt-72 bx-flashing'>pagina en construccion disculpe las molestias ocacionadas</h1>
+    </div>
     </>
   )
 }
