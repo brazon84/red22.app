33596683fcc67be4f8f9c8d4a891eebc560a7a28
Jose Gregorio Brazon Lopez <abg.brazon@gmail.com>
2024-03-27 15:27:32 -0400

    veamos

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-slate-800'>
+    <div className='bg-slate-800 bottom-0'>
 <section className='p-3'>
     <div className='text-white font-bold justify-center flex text-xs'>
       <h3 className='justify-center'>Copyright © 2004 –La Red 2022, C.A. Rif: J-50222218-9 Habilitacion Conatel- HGST-00733. Todos los derechos reservados.</h3>
