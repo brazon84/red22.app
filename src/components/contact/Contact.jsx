c0d8320903dfcdf1e2fd0ad7800d92385a465ed8
Jose Gregorio Brazon Lopez <abg.brazon@gmail.com>
2024-04-18 08:26:53 -0400

    contac

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Construccion from "../../assets/image/construccion.jpg"
+import React from 'react';
+import Construccion from "../../assets/image/construccion.jpg";
 
 const Contact = () => {
   return (
-    <div className='m-5 flex justify-center mb-24 h-[490px]'>
-      <img src={Construccion} alt="construccion" />
+    <div className='m-5 flex justify-center mb-24'>
+      <img 
+        src={Construccion} 
+        alt="construccion" 
+        className="w-full max-w-xl h-[490px]"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
